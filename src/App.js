a60f77107cf96ab5843ee99744545fc408821a59
kakarot987 <mayankit3@gmail.com>
2024-05-02 14:05:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect, Provider } from 'react-redux'; // Add Provider import
+import {legacy_createStore as createStore, applyMiddleware } from 'redux'; // Add createStore import
+import {thunk} from 'redux-thunk';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import rootReducer from './store/reducers/rootReducer'; // Import your root reducer
+import { setToken } from './store/actions/sessionActions';
+import { fetchUser } from './store/actions/userActions';
+
+import Spinner from './components/spinner/spinner';
+import LeftSection from './containers/leftSection/leftSection';
+import MainSection from './containers/mainSection/mainSection';
+import RightSection from './containers/rightSection/rightSection';
+
+import Login from './spotify/login';
+import WebPlaybackReact from './spotify/webPlayback';
+
+window.onSpotifyWebPlaybackSDKReady = () => {};
+
+class App extends Component {
+  state = {
+    playerLoaded: false,
+  };
+
+  componentDidMount() {
+    const token = Login.getToken();
+    if (!token) {
+      Login.logInWithSpotify();
+    } else {
+      this.setState({ token: token });
+      // this.props.setToken(token);
+      // this.props.fetchUser();
+    }
+  }
+
+  render() {
+    let webPlaybackSdkProps = {
+      playerName: 'Spotify React Player',
+      playerInitialVolume: 1.0,
+      playerRefreshRateMs: 1000,
+      playerAutoConnect: true,
+      onPlayerRequestAccessToken: () => this.state.token,
+      onPlayerLoading: () => {},
+      onPlayerWaitingForDevice: () => {
+        this.setState({ playerLoaded: true });
+      },
+      onPlayerError: (e) => {
+        console.log(e);
+      },
+      onPlayerDeviceSelected: () => {
+        this.setState({ playerLoaded: true });
+      },
+    };
+
+    return (
+      <div className='app'>
+        <WebPlaybackReact {...webPlaybackSdkProps}>
+          <Spinner loading={!this.state.playerLoaded}>
+            <LeftSection />
+            <MainSection />
+            <RightSection />
+          </Spinner>
+        </WebPlaybackReact>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    token: state.sessionReducer.token,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  setToken: (token) => dispatch(setToken(token)),
+  fetchUser: () => dispatch(fetchUser()),
+});
+
+// Remove default export
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// Create Redux store with rootReducer and apply middleware
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// Wrap App component with Provider and provide Redux store
+const AppWithStore = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+export default AppWithStore;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import rootReducer from './store/reducers/rootReducer'; // Import your root reducer
+import { Provider } from 'react-redux';
+import {legacy_createStore as createStore, applyMiddleware } from 'redux';
+import {thunk} from 'redux-thunk';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+const rootElement = 
+document.getElementById('root');
+const root = 
+createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </Provider>,
+)
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// );
+//   document.getElementById('root')
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
